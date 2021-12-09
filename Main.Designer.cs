e8508e458702f9946ef93ad5d8cd9d70e872c25f
Yonka 2019 <yonka2003@gmail.com>
2021-12-10 01:01:41 +0200

    Добавьте файлы проекта.

---
+++ Main.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace AsmHW_SmartRename
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.MainPanel.Controls.Add(this.label1);
+            this.MainPanel.Location = new System.Drawing.Point(12, 12);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(228, 143);
+            this.MainPanel.TabIndex = 0;
+            this.MainPanel.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainPanel_DragDrop);
+            this.MainPanel.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainPanel_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Drag and drop the .zip file to rename";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 167);
+            this.Controls.Add(this.MainPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace AsmHW_SmartRename
+{
+    public partial class Main : Form
+    {
+        private const string hw_path = @"D:\Magshimim\Magshimim-Assembly";
+        private const string hw_number_pattern = @"^Lesson(\d+)$";
+        private static Regex hw_number_regex = new Regex(hw_number_pattern);
+
+        public Main()
+        {
+            InitializeComponent();
+            GetLastHW();
+        }
+        private void GetLastHW()
+        {
+            int hw_num;
+
+            string[] _hw_folders = Directory.GetDirectories(hw_path);
+            List<string> hw_folders = new List<string>();
+            hw_folders.AddRange(_hw_folders);
+            string[] sorted = hw_folders
+                            .OrderBy(folderPath => hw_number_regex.Matches(Path.GetFileName(folderPath))[0].Value)
+                            .ToArray();
+
+        }
+        public static string GetFolder(string input)
+        {
+            MatchCollection mc = hw_number_regex.Matches(input);
+
+            if (mc.Count != 0)
+                return mc[0].Value;
+            else
+                return "Lesson0";
+
+        }
+
+        private void MainPanel_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string file = e.Data.GetData(DataFormats.FileDrop) as string;
+
+            }
+        }
+
+        private void MainPanel_DragEnter(object sender, DragEventArgs e) => e.Effect = DragDropEffects.All;
+
+    }
+}
