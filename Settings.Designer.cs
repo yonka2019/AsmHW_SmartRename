1bec04d1b75a52e0d4596dfc82fe024163f9ba89
Yonka 2019 <yonka2003@gmail.com>
2021-12-21 20:19:34 +0200

    asf

--- Settings.Designer.cs
+++ Settings.Designer.cs
@@ -43,16 +43,16 @@
             // FirstNameTB
             // 
             this.FirstNameTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.FirstNameTB.Location = new System.Drawing.Point(189, 21);
+            this.FirstNameTB.Location = new System.Drawing.Point(210, 21);
             this.FirstNameTB.Name = "FirstNameTB";
-            this.FirstNameTB.Size = new System.Drawing.Size(178, 26);
+            this.FirstNameTB.Size = new System.Drawing.Size(279, 26);
             this.FirstNameTB.TabIndex = 0;
             // 
             // FirstNameLabel
             // 
             this.FirstNameLabel.AutoSize = true;
             this.FirstNameLabel.Font = new System.Drawing.Font("Open Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.FirstNameLabel.Location = new System.Drawing.Point(96, 21);
+            this.FirstNameLabel.Location = new System.Drawing.Point(117, 21);
             this.FirstNameLabel.Name = "FirstNameLabel";
             this.FirstNameLabel.Size = new System.Drawing.Size(87, 22);
             this.FirstNameLabel.TabIndex = 1;
@@ -62,7 +62,7 @@
             // 
             this.HWPathLabel.AutoSize = true;
             this.HWPathLabel.Font = new System.Drawing.Font("Open Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.HWPathLabel.Location = new System.Drawing.Point(57, 85);
+            this.HWPathLabel.Location = new System.Drawing.Point(78, 85);
             this.HWPathLabel.Name = "HWPathLabel";
             this.HWPathLabel.Size = new System.Drawing.Size(126, 22);
             this.HWPathLabel.TabIndex = 3;
@@ -71,16 +71,16 @@
             // HWPathTB
             // 
             this.HWPathTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.HWPathTB.Location = new System.Drawing.Point(189, 83);
+            this.HWPathTB.Location = new System.Drawing.Point(210, 83);
             this.HWPathTB.Name = "HWPathTB";
-            this.HWPathTB.Size = new System.Drawing.Size(178, 26);
+            this.HWPathTB.Size = new System.Drawing.Size(279, 26);
             this.HWPathTB.TabIndex = 2;
             // 
             // LastNameLabel
             // 
             this.LastNameLabel.AutoSize = true;
             this.LastNameLabel.Font = new System.Drawing.Font("Open Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.LastNameLabel.Location = new System.Drawing.Point(98, 53);
+            this.LastNameLabel.Location = new System.Drawing.Point(119, 53);
             this.LastNameLabel.Name = "LastNameLabel";
             this.LastNameLabel.Size = new System.Drawing.Size(85, 22);
             this.LastNameLabel.TabIndex = 5;
@@ -89,16 +89,16 @@
             // LastNameTB
             // 
             this.LastNameTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.LastNameTB.Location = new System.Drawing.Point(189, 51);
+            this.LastNameTB.Location = new System.Drawing.Point(210, 51);
             this.LastNameTB.Name = "LastNameTB";
-            this.LastNameTB.Size = new System.Drawing.Size(178, 26);
+            this.LastNameTB.Size = new System.Drawing.Size(279, 26);
             this.LastNameTB.TabIndex = 4;
             // 
             // FoldersPatternLabel
             // 
             this.FoldersPatternLabel.AutoSize = true;
             this.FoldersPatternLabel.Font = new System.Drawing.Font("Open Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.FoldersPatternLabel.Location = new System.Drawing.Point(64, 117);
+            this.FoldersPatternLabel.Location = new System.Drawing.Point(85, 117);
             this.FoldersPatternLabel.Name = "FoldersPatternLabel";
             this.FoldersPatternLabel.Size = new System.Drawing.Size(119, 22);
             this.FoldersPatternLabel.TabIndex = 7;
@@ -107,14 +107,14 @@
             // FoldersPatternTB
             // 
             this.FoldersPatternTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.FoldersPatternTB.Location = new System.Drawing.Point(189, 115);
+            this.FoldersPatternTB.Location = new System.Drawing.Point(210, 115);
             this.FoldersPatternTB.Name = "FoldersPatternTB";
-            this.FoldersPatternTB.Size = new System.Drawing.Size(178, 26);
+            this.FoldersPatternTB.Size = new System.Drawing.Size(279, 26);
             this.FoldersPatternTB.TabIndex = 6;
             // 
             // SaveButton
             // 
-            this.SaveButton.Location = new System.Drawing.Point(156, 152);
+            this.SaveButton.Location = new System.Drawing.Point(253, 152);
             this.SaveButton.Name = "SaveButton";
             this.SaveButton.Size = new System.Drawing.Size(102, 29);
             this.SaveButton.TabIndex = 8;
@@ -126,7 +126,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(425, 193);
+            this.ClientSize = new System.Drawing.Size(567, 191);
             this.Controls.Add(this.SaveButton);
             this.Controls.Add(this.FoldersPatternLabel);
             this.Controls.Add(this.FoldersPatternTB);
